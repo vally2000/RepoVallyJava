705406aa8c2b6f454de51cd3a543007b5dea2234
vally2000 <117976427+vally2000@users.noreply.github.com>
2022-11-11 10:58:49 +0100

    11-11-22 Esercizio4

---
+++ Es4.java
@@ -0,0 +1,34 @@
+public class Es4 {
+    public static void main (String [] args){
+
+                 
+        double b = 'b';         //dichiaro le variabili
+        double a = 123.34;
+        int c = 1;
+
+        double somma;           //dichiaro la variabile che "conterrà" la somma
+        double divisotre;       //dichiaro la variabile che "conterrà" la divisione per 3
+        somma = b+a+c;          //eseguo la somma
+        divisotre = somma/3;    //eseguo la divisione
+
+        boolean val1 =true;     //dichiaro le variabili booleane
+        boolean val2 = false;
+
+        if (val1==!val2){       //dichiaro la condizione, se le variabili coincidono stamperà "evviva"
+            System.out.println("evviva");
+        }
+        else {
+        }
+
+        System.out.println();          //stampa a video tutte le operazioni
+        System.out.println(somma);
+        System.out.println();
+        System.out.println(divisotre);
+
+
+
+
+
+
+    }
+}
